30d7d8982c957c944f956d9f3120ca18da047698
Patrik Johansson <Patrik.Viby@gmail.com>
2017-02-17 11:10:03 +0100

    Fixed logical error & added height / width constraint

--- src/BMemory.hs
+++ src/BMemory.hs
@@ -24,26 +24,26 @@ buildMemory arr progLines = do
   return ()
     where
       buildArrayAux :: IOArray Position Char -> [String] -> Int -> IO ()
+      buildArrayAux _ _ y | y == height = error $ "Too many lines"
       buildArrayAux _ [] _ = return ()
       buildArrayAux arr (s:ss) y = do
         buildArrayAux2 arr (0, y) s
         buildArrayAux arr ss (y + 1)
 
       buildArrayAux2 :: IOArray Position Char -> Position -> String -> IO ()
+      buildArrayAux2 _ (x, y) _ | x == width = error $ "Too many characters on line " ++ show (y)
       buildArrayAux2 _ _ "" = return ()
       buildArrayAux2 arr (x, y) (c:cs) = do
         putValue arr (x, y) c
         buildArrayAux2 arr (x + 1, y) cs
 
 
-getValue arr (x, y) = do
-  (minpos, maxpos) <- getBounds arr
-  if (x, y) < minpos || (x, y) > maxpos
-    then return ' '
+getValue arr (x, y) =
+  if x < 0 || x >= width || y < 0 || y >= height
+    then return ' ' --allow illegal get / put? Or raise error?
     else readArray arr (x, y)
 
-putValue arr (x, y) val = do
-  (minpos, maxpos) <- getBounds arr
-  if (x, y) < minpos || (x, y) > maxpos
+putValue arr (x, y) val = 
+  if x < 0 || x >= width || y < 0 || y >= height
     then return ()
     else writeArray arr (x, y) val
