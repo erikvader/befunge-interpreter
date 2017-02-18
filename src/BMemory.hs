8cf528783c5e997a8821a3249dccc77dac7c272d
Staffan Annerwall <staffan.annerwall@gmail.com>
2017-02-18 09:05:19 +0100

    Improve bounds checking in BMemory.hs

--- src/BMemory.hs
+++ src/BMemory.hs
@@ -20,26 +20,35 @@ putValue :: IOArray Position Char -> Position -> Char -> IO ()
 --------------------------------------------------------------------------------
 
 buildMemory arr progLines = do
-  buildArrayAux arr progLines 0
+  (minpos, maxpos) <- getBounds arr
+  buildArrayAux arr progLines minpos maxpos 0
   return ()
     where
-      buildArrayAux :: IOArray Position Char -> [String] -> Int -> IO ()
-      buildArrayAux _ _ y | y == height = error $ "Too many lines"
-      buildArrayAux _ [] _ = return ()
-      buildArrayAux arr (s:ss) y = do
-        buildArrayAux2 arr (0, y) s
-        buildArrayAux arr ss (y + 1)
+      buildArrayAux :: IOArray Position Char -> [String] -> Position -> Position -> Int -> IO ()
+      buildArrayAux _ _ (_, miny) (_, maxy) y
+        | y < miny || y > maxy = return ()
+      buildArrayAux _ [] _ _ _ = return ()
+      buildArrayAux arr (s:ss) minpos@(minx, miny) maxpos@(maxx, maxy) y = do
+        buildArrayAux2 arr minx maxx (0, y) s
+        buildArrayAux arr ss minpos maxpos (y + 1)
 
-      buildArrayAux2 :: IOArray Position Char -> Position -> String -> IO ()
-      buildArrayAux2 _ (x, y) _ | x == width = error $ "Too many characters on line " ++ show (y)
-      buildArrayAux2 _ _ "" = return ()
-      buildArrayAux2 arr (x, y) (c:cs) = do
+      buildArrayAux2 :: IOArray Position Char -> Int -> Int -> Position -> String -> IO ()
+      buildArrayAux2 _ minx maxx (x, _) _
+        | x < minx || x > maxx = return ()
+      buildArrayAux2 _ _ _ _ "" = return ()
+      buildArrayAux2 arr minx maxx (x, y) (c:cs) = do
         putValue arr (x, y) c
-        buildArrayAux2 arr (x + 1, y) cs
+        buildArrayAux2 arr minx maxx (x + 1, y) cs
 
 
-getValue arr (x, y) =
-    readArray arr (mod x width, mod y height)
+getValue arr (x, y) = do
+  ((minx, miny), (maxx, maxy)) <- getBounds arr
+  let w = maxx - minx + 1
+  let h = maxy - miny + 1
+  readArray arr (mod x w, mod y h)
 
-putValue arr (x, y) val = 
-    writeArray arr (mod x width, mod y height) val
+putValue arr (x, y) val = do
+  ((minx, miny), (maxx, maxy)) <- getBounds arr
+  let w = maxx - minx + 1
+  let h = maxy - miny + 1
+  writeArray arr (mod x w, mod y h) val
